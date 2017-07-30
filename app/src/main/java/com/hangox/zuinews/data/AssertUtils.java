78113bf213d84a5af74967c2fbea83c2e7925a93
HangoX <liang.hanguang93@gmail.com>
2017-07-30 23:30:13 +0800

    添加了assert操作的类

---
+++ app/src/main/java/com/hangox/zuinews/data/AssertUtils.java
@@ -0,0 +1,22 @@
+package com.hangox.zuinews.data;
+
+import android.content.Context;
+
+import org.greenrobot.essentials.io.IoUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Created With Android Studio
+ * User HangoX
+ * Date 2017/7/30
+ * Time 下午10:53
+ */
+
+public class AssertUtils {
+    public static String getStringFromAssert(Context context,String fileName) throws IOException {
+        InputStream stream = context.getResources().getAssets().open("fileName");
+        return new String(IoUtils.readAllBytes(stream));
+    }
+}
