99364834ded64def225531696603e733b4bc927e
47 <liang.hanguang93@gmail.com>
2017-08-03 10:42:45 +0800

    添加Api调用的key文件

---
+++ app/src/main/java/com/hangox/zuinews/AppConfig.java
@@ -0,0 +1,21 @@
+package com.hangox.zuinews;
+
+/**
+ * Created With Android Studio
+ * User hangox
+ * Date 2017/7/5
+ * Time 下午10:00
+ */
+
+public class AppConfig  {
+
+    /**
+     *易源API 应用ID
+     */
+    public static final String SHOW_API_ID = BuildConfig.SHOW_APP_ID;
+
+    /**
+     * 易源API 密钥
+     */
+    public static final String SHOW_API_SECRET = BuildConfig.SHOW_APP_SECRET;
+}
